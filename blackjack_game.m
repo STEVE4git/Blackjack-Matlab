4fd127040751ca9e701b3332efd53979d8fd08aa
STEVE4 <stephenmartinemail20030@gmail.com>
2022-06-23 10:06:50 -0400

    Added needed image files

--- blackjack_game.m
+++ blackjack_game.m
@@ -1,5 +1,7 @@
 %Blackjack game. Reads user input, allows betting
 %Written by: Stephen Martin, 
+%This is the main file for running the game and contains the important logic regarding how the game works
+%Functions are run from the main while loop stopping when the user runs out of money
 
 function [] = main() % The  main entry point of the matlab function
 
@@ -25,7 +27,7 @@ if human_money < chips_cost*how_many_chips
     prompt_error = "You don't have enough money to buy the chips!";
     disp(prompt_error);
     prompt_resolve = "Do you want to try buying chips again? Type yes or no.";
-    error_string = input(prompt_resolve, "s");
+    error_string = input(prompt_resolve, "s");    %BUG_FIX: Replace this with actual graphics code it can't be read from the command prompt.
     lower_string = lower(error_string);
     
     if strcmp(lower_string, "yes")
@@ -42,14 +44,14 @@ function cards = generate_cards()
 
 rng = ('shuffle');
 card_value_size = length(CARD_VALUE);
-card_type_size = length(CARD_TYPE);
+card_type_size = length(CARD_TYPE); 
 
 
-rand_card_index = randi([1 13],52);
+rand_card_index = randi([1 13],52);  
 card_type_index = randi([1 4], 52);
 
 card_column_vector(:,1)=rand_card_index;
-card_column_vector(:,2)=card_type_index;
+card_column_vector(:,2)=card_type_index; %These will represent the cards based on the CONSTANTS at the bottom
 
 cards = card_column_vector;    %Stores the card type and value as a column vector. The key is at the bottom of the file for consistenty
 
