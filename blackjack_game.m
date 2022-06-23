a8eaad87a79fce93ea04f6603edf0b535845fbba
STEVE4 <stephenmartinemail20030@gmail.com>
2022-06-23 08:49:11 -0400

    Inital design commit

--- blackjack_game.m
+++ blackjack_game.m
@@ -1,1 +1,26 @@
-%Blackjack game
+%Blackjack game. Reads user input, allows betting, graphics.__amd64
+
+
+function entry = main() % The  main entry point of the matlab function
+
+human_money =  5000
+chips = 72
+current_cards = 52
+
+while 0 < human_money 
+
+
+
+
+end
+
+
+
+function cards = generate_cards(num_of_cards)
+
+
+
+end
+
+
+function 
