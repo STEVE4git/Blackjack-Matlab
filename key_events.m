ddc9ac92336238b7449135c0a709e4f10f2d59c3
Aryapatel1504 <108073212+Aryapatel1504@users.noreply.github.com>
2022-06-29 08:47:48 -0400

    Add files via upload

--- key_events.m
+++ key_events.m
@@ -1,3 +1,13 @@
-%file for getting/reading all keyboard events
-%this controls all keydown/keyup events from user input
-
+function n =  key_events(src,event)
+  %key events decodes the user inputs from keyboard
+  %input arguments
+  %src - callback function
+  %event - callback function
+  %output arguments
+  %quitgame - sets quitgame to true if user wants to quit game
+global quitgame = false;
+switch  event.key
+  case 'q'
+    quitgame = true;
+  end
+  end
