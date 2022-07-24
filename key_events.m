72ec2e2baba9e91a18c6b1b068b02d77fe0b718b
Aryapatel1504 <108073212+Aryapatel1504@users.noreply.github.com>
2022-07-24 13:52:54 -0400

    Add files via upload

--- key_events.m
+++ key_events.m
@@ -1,22 +1,30 @@
-function n =  key_events(src,event)
+function [] =  key_events(~,event)
   %key events decodes the user inputs from keyboard
   %input arguments
-  %src - callback function
   %event - callback function
   %output arguments
-  
+ 
+  k = lower(event.Key);
+ switch k
+            case 'return'
+            bet_string('yes'); %returns true if user wants to enter game
+
+            case 'yes'      %user hits yes for  buying chips and stores the value in bet
+            bet = scene.Key;
+            prompt_bet(bet)
+
+            case 'no'      %user hits no to change his bets
+            bet_string('no')
+            
+            case 'escape'
+            quit_game(true)
+
+            otherwise   %return false if wrong key is pressed
+            enter_game(false);
+            quit_game(false);
 
 
-  switch event.Key
 
-  case 'return' %checks for return to be pressed to continue the game
-  entergame = true;
+end
 
-  case 'q'      %checks whether the player wants to quit
-  quitgame =  true;
-
-  case (event.Key!='return') %takes the input from the screen and stores it in n
-     n = event.Key;
-     return ;
-   end
-   end
+end
